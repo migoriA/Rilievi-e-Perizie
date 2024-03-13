dcaa6a5c5d1822919607a31437e11d4b73e493a9
Migori <92369545+migoriA@users.noreply.github.com>
2024-03-13 22:30:31 +0100

    LogIn page and redirect to the home page done

---
+++ Admin/src/app/components/login/login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  constructor(protected router: Router) {  }
+}
