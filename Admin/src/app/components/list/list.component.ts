fb62d25517bdb41c440ce2e4e352a27ce85f14ac
Migori <92369545+migoriA@users.noreply.github.com>
2024-04-04 21:47:52 +0200

    List style for the perizie

--- Admin/src/app/components/list/list.component.ts
+++ Admin/src/app/components/list/list.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RowComponent } from '../row/row.component';
 
 @Component({
     selector: 'app-list',
     templateUrl: './list.component.html',
     styleUrl: './list.component.scss',
-    standalone: true
+    standalone: true,
+    imports: [RowComponent]
 })
 export class ListComponent {
 
